4d1f9df31f89095405ac2688ffe572629a4cd48a
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-18 18:30:01 +0530

    operators

---
+++ area.py
@@ -0,0 +1,5 @@
+circumference=float(input("enter the circumference:"))
+r=circumference/(2*3.14)
+print(r)
+area=3.14*r*r
+print("area is:",area)
---
+++ si.py
@@ -0,0 +1,7 @@
+p=float(input("enter the principle amount:"))
+r=float(input("enter the rate of interest:"))
+t=float(input("enter the time:"))
+si=p*r*t/100
+print("simple interest is:",si)
+amount=si+p
+print("amount is:",amount)
